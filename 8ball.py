e832b56e365fc7b55d9610855d2041011053dcf3
Rahul Bonnerjee <Rahul.Bonnerjee@gmail.com>
2018-01-23 14:23:27 -0500

    Making 8-ball check infinite

--- 8ball.py
+++ 8ball.py
@@ -1,7 +1,7 @@
 a = ["Without a doubt","Most likely"," Better not tell you now", "Don't count on it"]
 
 q = input("What is the question you'd like to present the 8-ball?")
-if q[-1] != "?":
+while q[-1] != "?":
     print("Try again, please finish your question with a ? symbol")
     q = input("What is the question you'd like to present the 8-ball?")
 
